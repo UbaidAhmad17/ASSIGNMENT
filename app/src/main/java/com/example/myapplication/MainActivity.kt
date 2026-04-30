6b2a9355d0ac1b4ded8fff8d251f9b2d326deb01
Ubaid Ahmad <oceanicp3@gmail.com>
2026-05-01 01:44:49 +0500

    initial commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,91 +1,1 @@
-package com.example.myapplication
-
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.myapplication.ui.theme.MyApplicationTheme
-
-class MainActivity : ComponentActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-
-        val people = listOf("John", "Jack", "Max", "Lewis","John", "Jack", "Max", "Lewis","John", "Jack", "Max", "Lewis")
-
-        setContent {
-            MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-                    Column(modifier = Modifier.padding(innerPadding)) {
-
-                        Greeting(name = "Android")
-
-                        LazyColumn {
-                            items(people) {
-                                ListItem(it)
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier.padding(16.dp)
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
-}
-
-@Composable
-fun ListItem(name: String) {
-
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(12.dp)
-    ) {
-        Row(
-            modifier = Modifier.padding(12.dp)
-        ) {
-
-            Image(
-                painter = painterResource(id = R.drawable.person),
-                contentDescription = "Person",
-                modifier = Modifier
-                    .width(100.dp)
-                    .height(100.dp)
-            )
-
-            Text(
-                text = name,
-                modifier = Modifier.padding(start = 24.dp)
-            )
-        }
-    }
-}
+package com.example.myapplicationimport android.os.Bundleimport androidx.activity.ComponentActivityimport androidx.activity.compose.setContentimport androidx.activity.enableEdgeToEdgeimport androidx.compose.foundation.Imageimport androidx.compose.foundation.layout.*import androidx.compose.foundation.lazy.LazyColumnimport androidx.compose.foundation.lazy.itemsimport androidx.compose.material3.Cardimport androidx.compose.material3.Scaffoldimport androidx.compose.material3.Textimport androidx.compose.runtime.Composableimport androidx.compose.ui.Modifierimport androidx.compose.ui.res.painterResourceimport androidx.compose.ui.tooling.preview.Previewimport androidx.compose.ui.unit.dpimport com.example.myapplication.ui.theme.MyApplicationThemeclass MainActivity : ComponentActivity() {    override fun onCreate(savedInstanceState: Bundle?) {        super.onCreate(savedInstanceState)        enableEdgeToEdge()        val people = listOf("John", "Jack", "Max", "Lewis","John", "Jack", "Max", "Lewis","John", "Jack", "Max", "Lewis")        setContent {            MyApplicationTheme {                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->                    Column(modifier = Modifier.padding(innerPadding)) {                        Greeting(name = "Android")                        LazyColumn {                            items(people) {                                ListItem(it)                            }                        }                    }                }            }        }    }}@Composablefun Greeting(name: String, modifier: Modifier = Modifier) {    Text(        text = "Hello $name!",        modifier = modifier.padding(16.dp)    )}@Preview(showBackground = true)@Composablefun GreetingPreview() {    MyApplicationTheme {        Greeting("Android")    }}@Composablefun ListItem(name: String) {    Card(        modifier = Modifier            .fillMaxWidth()            .padding(12.dp)    ) {        Row(            modifier = Modifier.padding(12.dp)        ) {            Image(                painter = painterResource(id = R.drawable.person),                contentDescription = "Person",                modifier = Modifier                    .width(100.dp)                    .height(100.dp)            )            Text(                text = name,                modifier = Modifier.padding(start = 24.dp)            )        }    }}
